6e68838c26fae0f57fd8b44f25bd5780415ece9a
x_ujxxs <jakuresi@mail.ru>
2025-02-04 00:14:39 +0400

    Added MDC to logging info

---
+++ task/src/main/java/io/tasks_tracker/task/component/MdcLoggingInterceptor.java
@@ -0,0 +1,46 @@
+package io.tasks_tracker.task.component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.slf4j.MDC;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.tasks_tracker.task.service.AuthenticationService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MdcLoggingInterceptor extends OncePerRequestFilter 
+{
+    private final AuthenticationService authenticationService;
+
+    public MdcLoggingInterceptor(AuthenticationService authenticationService)
+    {
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException
+    {
+        MDC.put("requestId", UUID.randomUUID().toString());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        
+        if(authentication != null && authentication.isAuthenticated()
+            && authentication.getDetails() instanceof Long
+        ) {
+            MDC.put("userId", authenticationService.getUserId(authentication).toString());
+        }
+
+        filterChain.doFilter(request, response);
+
+        MDC.clear();
+    }
+}
