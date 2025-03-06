069578631e3a0ce35f8913ebc610c6d45d9d00a5
x_ujxxs <jakuresi@mail.ru>
2025-03-06 22:37:20 +0400

    Added tests for service classes

--- task/src/test/java/io/tasks_tracker/task/service/SubtaskServiceTests.java
+++ task/src/test/java/io/tasks_tracker/task/service/SubtaskServiceTests.java
@@ -2,7 +2,6 @@ package io.tasks_tracker.task.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
